017b3f5cc62589091a8d50520f50bcb8a4c6bd6f
Luiz Spengler <Luiz.cmspengler@gmail.com>
2024-05-31 21:11:16 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import Calculadora from './components/Calculadora.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <Calculadora />
+  </div>
+</template>
+
+<style>
+input {
+  max-width: 420px;
+}
+
+option {
+  background-color: #b3e4e1c5;
+}
+</style>
